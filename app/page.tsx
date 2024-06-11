7f495e8edb9996332e22f333c932ebe6b12be442
[AtharvPorate1] <www.atharvporate254@gmail.com>
2024-06-11 21:00:38 +0530

    Local setup with db completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { prisma } from "@/utils/db";
 import { auth, currentUser } from "@clerk/nextjs/server";
 import Welcome from "@/components/Welcome";
 import {
@@ -8,14 +9,49 @@ import {
   SignedOut,
   
 } from '@clerk/nextjs'
+import {redirect} from "next/navigation";
 
 
-export default function Home() {
+const createUserOnDB = async ()=>{
+  const user = await currentUser();
+  console.log(user);
+  console.log("clerkId : ", user?.id);
+
+  const match = await prisma.user.findUnique({
+      where: {
+          clerkId: user?.id as string
+      }
+  });
+  if (match) console.log('User already exists');
+
+  if(!match){
+      await prisma.user.create({
+          data: {
+              clerkId: user?.id as string,
+              email: user?.emailAddresses[0].emailAddress as string,
+            
+  } 
+      });
+  }
+ 
+
+}
+
+
+
+
+export default async function Home() {
 
   const { userId } = auth();
 
   if (userId) {
     console.log("userId", userId);
+    await createUserOnDB();
+    console.log("User created")
+
+    redirect("/dashboard");
+    
+   
   }
 
   
--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
+
+
 const Welcome = () => {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">The Main Page</h1>
-        <p>Welcome to the main page.</p>
-      
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <h1 className="text-4xl font-bold">The Main Page</h1>
+        {/* <p className="text-2xl font-bold">Welcome to the main page.</p> */}
+
+        
+      <Link href="/dashboard">
+          <button className="text-2xl">
+            Move to dashboard
+          </button>
+    </Link>
     </main>
     );
 }
